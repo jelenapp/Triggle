89ae1eccbb89855d4f55f2a571ba229081d12ec2
Djordje Stevanovic <djordjest@elfak.rs>
2024-12-01 19:01:22 +0100

    interfejs muke

--- Triggle/igra.py
+++ Triggle/igra.py
@@ -22,25 +22,58 @@ class Igra:
 
         return tabla
 
-    def prikaziStanje(self): #interfejs
+    def prikaziStanje(self):
         max_red = max(red for red, _ in self.tabla)
         prikaz = []
-        
+        razmak_izmedju_tacaka = 6
+
+        # Priprema horizontalnih gumica
+        horizontalne_gumice = set()
+        dijagonalne_gumice = []
+
+        for gumica in self.gumice:
+            if gumica[0][0] == gumica[1][0]:  # horizontalna gumica (-)
+                red, kolona1 = gumica[0]
+                _, kolona2 = gumica[1]
+                for kolona in range(min(kolona1, kolona2), max(kolona1, kolona2)):
+                    horizontalne_gumice.add((red, kolona, kolona + 1))
+            elif gumica[0][1] == gumica[1][1]:  # dijagonalna gumica (/)
+                dijagonalne_gumice.append(gumica)
+
         for red in range(1, max_red + 1):
             kolone = sorted(kolona for r, kolona in self.tabla if r == red)
-            
-            if red <= max_red // 2 + 1:  
-                razmaci = max_red - red
-            else: 
-                razmaci = red - (max_red // 2 - (self.n-1)) 
-            
-            red_prikaza = ' ' * (razmaci * 2)  
-            
-            for kolona in kolone:
-                red_prikaza += '●   '
-            
+
+            if red <= max_red // 2 + 1:
+                razmaci = max_red - red  # gornji deo
+            else:
+                razmaci = red - (max_red // 2 - 2)  # donji deo
+
+            red_prikaza = ' ' * (razmaci * razmak_izmedju_tacaka // 2)  # prazan prostor na pocetku
+            razmak_red = list(' ' * (len(red_prikaza) + (len(kolone) - 1) * razmak_izmedju_tacaka))  # linija za razmak
+
+            # iscrtavanje tacaka
+            for i, kolona in enumerate(kolone):
+                red_prikaza += '●'
+                if i < len(kolone) - 1:  # dodaj crtice između tacaka (horizontalne gumice (-))
+                    if (red, kolona, kolone[i + 1]) in horizontalne_gumice:
+                        red_prikaza += '-' * razmak_izmedju_tacaka
+                    else:
+                        red_prikaza += ' ' * razmak_izmedju_tacaka
+
             prikaz.append(red_prikaza.rstrip())
-        
+
+            # provera za dijagonalne gumice (/)
+            for gumica in dijagonalne_gumice:
+                pocetak, kraj = gumica
+                if pocetak[0] <= red < kraj[0]:  # ako smo u opsegu dijagonalne gumice
+                    trenutna_kolona = pocetak[1]
+                    razmak_do_pocetka = razmaci * (razmak_izmedju_tacaka // 2) + (trenutna_kolona - kolone[0]) * razmak_izmedju_tacaka - razmak_izmedju_tacaka // 2
+                    if 0 <= razmak_do_pocetka < len(razmak_red):
+                        razmak_red[razmak_do_pocetka] = '  /'
+
+            prikaz.append(''.join(razmak_red).rstrip())  # dodaj prazan red ili red s dijagonalnim gumicama
+
+        # Prikazivanje rezultata
         for red in prikaz:
             print(red)
 
@@ -88,7 +121,7 @@ class Igra:
             return False
         
         # if pozicija in self.tabla:
-        #     print(self.tabla) ovo ti je ako ces da pogledas koordinate 
+        #     print(self.tabla) #ovo ti je ako ces da pogledas koordinate 
 
         # provera validnosti smera
         validni_smerovi = {'D', 'DL', 'DD'}
@@ -98,77 +131,4 @@ class Igra:
 
         return True
 
-
-
-
-print("=============== Igra - n=3 =====================")
-
-
-igra = Igra(3)
-
-# igra.prikaziStanje() ovo ti je da stampa u konzoli sestougao, proveri slobodno i za n=4,5,6 blabla radi sve
-
-print(igra.izracunajUkupanBrojTrouglica()) #doso sam do formule da za n stranice ima n^2 * 6 trouglica, ovu fju koristimo gore u proveriKrajIgre
-print(igra.proveriPotez(((1, 5), 'D')))  # nije moguc za tablu stranice n=3 (False)
-igra.gumice.append([(1,1),(1,4)]) #dodajem gumice da vidim da li se posle resetuje kako treba
-print(f"gumice:{igra.gumice}")
-igra.trouglici['X'] = [[(1,2),(2,2),(2,3)]] #dodajem trouglice isto ono da vidim kasnije da li resetuje lepo
-print(f"trouglici:{igra.trouglici}")
-
-print("=============== Igra - n=4 =====================")
-
-igra.postaviPocetnoStanje(4) #ovo je reset i uz reset moze da se postavi nova velicina stranice (sad je n=4)
-print(igra.izracunajUkupanBrojTrouglica()) # ukupan broj trouglica je sada drugaciji zbog n=4 (bio je 54 sad je 96)
-print(f"gumice:{igra.gumice}") #gumice su isto vracene na prazan niz
-print(igra.proveriPotez(((1, 5), 'D')))  # sada je ovaj potez moguc jer je tabla veca (True)
-
-print("===============Provere pobednika=====================")
-
-igra.rezultat['X'] = 49  # više od polovine (49 > (96/2))
-igra.rezultat['O'] = 5
-kraj, pobednik = igra.proveriKrajIgre()
-print(f"Kraj igre: {kraj}, Pobednik: {pobednik}")
-
-igra.rezultat['X'] = 40
-igra.rezultat['O'] = 40
-kraj, pobednik = igra.proveriKrajIgre()
-print(f"Kraj igre: {kraj}, Pobednik: {pobednik}") #false zato sto je 40:40, jos se igra
-
-igra.rezultat['X'] = 48
-igra.rezultat['O'] = 48
-kraj, pobednik = igra.proveriKrajIgre()
-print(f"Kraj igre: {kraj}, Pobednik: {pobednik}")  #true zato sto je dostignut krajnji rezultat
-# u praksi jedino kada je rezultat['X'] + rezultat['O'] = broj trouglica je kad je rezultat neresen (48:48 -> 48+48=96 - kraj)
-# u svakom drugom slucaju gde rezultat prelazi 48 i biva recimo 49:47, 49:35, 49:10...igra se zavrsava jer je pobednik vec poznat
-# simple: neko od igraca presao polovinu trouglica? kraj igre. nijedan nije presao polovinu trouglica? nije kraj igre. 
-# oba nisu presli polovinu trouglica a zbir poena je max broj trouglica? kraj igre - nereseno
-
-print("===============Provere poteza=====================")
-
-print(igra.proveriPotez(((1, 5), 'D')))   # True
-print(igra.proveriPotez(((1, 6), 'D'))) #  false
-print(igra.proveriPotez(((1, 1), 'X')))   # false
-print(igra.proveriPotez((0, 'D')))        #  False (neispravan format poteza)
-
-
-
-# - Idemo redom: prvo ubaceno je gore self.covek_na_potezu = True i to je ono iz uslova zadatka da se bira ko prvi igra covek ili racunar
-# i ako je covek onda je True ako nije onda False, mada ne znam iskreno sta ce nam to ovde u prvoj fazi ali dobro
-# - Tabla je sada niz koordinata, vise ne cuvamo one gluposti gumice i trouglove za svaku koordinatu (stubic) <-- napraviTablu fja
-# - Prikazi stanje funkcija je u procesu jos i trenutno sam uspeo samo da napravim sestougao u konzoli na osnovu koordinata iz tabla[]
-# mozes da probas da odkomentarises u 'mainu' dole 'igra.prikaziStanje()' i da vidis sta se stampa, i isto mozes da probas i za Igra(4) ili bilo 
-# koju velicinu stranice, stampa ga lepo 
-# Btw to za interfejs nisam bio ni siguran da li treba da se pravi, ali kapiram valjda da treba da se pravi u konzoli da se prikaze, jer ne bi stavljali 
-# one slajdove mrtve sa interfejsom a mnogo je zajebano da se crta tako u konzoli
-# Tkd za to prikazi stanje tj crtanje treba da se doradi da se crtaju gumice i trouglici za neko proizvoljno stanje
-# tkd jebavao bih se sa tim sutra pa ako uspem uspeo sam, svakako nema nista drugo vise da se radi tolko
-# - Dalje imamo da su trouglici postali dictionary da bismo mogli da pratimo trouglice posebno za X i O jer kad stampas u konzoli jebiga 
-# ne stampas trouglice nego stampas 'X' i 'O' za svakog igraca posebno, tkd mora tako
-# - Ostale stvari, malo sam nesto doradio, skapirao sam formulu kako da se odredi broj trouglica na osnovu velicine stranice sestougla, imas sve gore
-# - Obrati paznju na ono kako detektujemo ko je pobednik, kad igra jos nije gotova, kad je nereseno itd, imas komentar gore u mainu simple je
-
-# E sad osim tog interfejsa koji treba da iscrtam nekako, nema toliko nista spec vise da se radi
-# Oni su stavljali u ovim zahtevima za fazu 1 kao izbor ko prvi igra, unos poteza ali bez odigravanja poteza itd, tkd ja kapiram da ne treba 
-#da postoji nikakva ono live interakcija u konzoli jos gde korisnik bira ko igra prvi itd nego sve to ide preko funkcija za sad, testiranje samo
-# Ako mislis da ipak treba nesto preko, pogledaj zahteve, uporedi code, i kazi mi.
 
